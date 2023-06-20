be32ce9c7d4d42f979f37fc68788b45a35362bad
Athirson Silva <athirsonarceus@gmail.com>
2023-06-20 19:37:45 -0300

    initial commit

---
+++ sales/src/test/java/com/graphql/graphqlchallenge/GraphqlChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.graphqlchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
