24f4eff6d08f85b6b73a33d777daed540bb8e19c
Athirson Silva <athirsonarceus@gmail.com>
2023-07-24 15:09:03 -0300

    modificated

--- product/src/main/java/com/carmazing/product/datasource/specification/SeriesSpecification.java
+++ product/src/main/java/com/carmazing/product/datasource/specification/SeriesSpecification.java
@@ -5,42 +5,37 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class SeriesSpecification extends BaseSpecification {
 
-    public static final String FIELD_NAME = "name";
-    public static final String FIELD_MANUFACTURER = "manufacturer";
+	public static final String FIELD_NAME = "name";
+	public static final String FIELD_MANUFACTURER = "manufacturer";
 
-    public static Specification<Series> seriesNameContainsIgnoreCase(String keyword) {
-        return (root, query, criteriaBuilder) -> {
-            return criteriaBuilder.like(
-                    criteriaBuilder.lower(root.get(FIELD_NAME)),
-                    contains(keyword.toLowerCase())
-            );
-        };
-    }
+	public static Specification<Series> seriesNameContainsIgnoreCase(String keyword) {
+		return (root, query, criteriaBuilder) -> {
+			return criteriaBuilder.like(
+					criteriaBuilder.lower(root.get(FIELD_NAME)),
+					contains(keyword.toLowerCase()));
+		};
+	}
 
-    public static Specification<Series> manufacturerNameContainsIgnoreCase(String keyword) {
-        return (root, query, criteriaBuilder) -> {
-            var joinManufacturer = root.join(FIELD_MANUFACTURER);
+	public static Specification<Series> manufacturerNameContainsIgnoreCase(String keyword) {
+		return (root, query, criteriaBuilder) -> {
+			var joinManufacturer = root.join(FIELD_MANUFACTURER);
 
-            return criteriaBuilder.like(
-                    criteriaBuilder.lower(joinManufacturer.get(
-                            ManufacturerSpecification.FIELD_NAME
-                    )),
-                    contains(keyword.toLowerCase())
-            );
-        };
-    }
+			return criteriaBuilder.like(
+					criteriaBuilder.lower(joinManufacturer.get(
+							ManufacturerSpecification.FIELD_NAME)),
+					contains(keyword.toLowerCase()));
+		};
+	}
 
-    public static Specification<Series> manufacturerOriginCountryContainsIgnoreCase(String keyword) {
-        return (root, query, criteriaBuilder) -> {
-            var joinManufacturer = root.join(FIELD_MANUFACTURER);
+	public static Specification<Series> manufacturerOriginCountryContainsIgnoreCase(String keyword) {
+		return (root, query, criteriaBuilder) -> {
+			var joinManufacturer = root.join(FIELD_MANUFACTURER);
 
-            return criteriaBuilder.like(
-                    criteriaBuilder.lower(joinManufacturer.get(
-                            ManufacturerSpecification.FIELD_ORIGIN_COUNTRY
-                    )),
-                    contains(keyword.toLowerCase())
-            );
-        };
-    }
+			return criteriaBuilder.like(
+					criteriaBuilder.lower(joinManufacturer.get(
+							ManufacturerSpecification.FIELD_ORIGIN_COUNTRY)),
+					contains(keyword.toLowerCase()));
+		};
+	}
 
 }
